fba498b7ea645ff51833f02e43212014ba6f5f75
Stewart Smith <stew@rtsmith.io>
2021-06-04 20:04:30 -0400

    Testing finger walking.

--- scripts/main.js
+++ scripts/main.js
@@ -49,7 +49,8 @@ camera,
 scene,
 renderer,
 controls,
-stats
+stats,
+world
 
 function setupThree(){
 
@@ -79,13 +80,22 @@ function setupThree(){
 		far 
 	)
 	camera.position.set( 0, userHeight, 6 )
-
+window.camera = camera
 	
 	//  Scene.
 	//  https://threejs.org/docs/#api/en/scenes/Scene
 
 	scene = new THREE.Scene()
 	scene.add( camera )
+
+
+	//  We’d also like a “world” group to attach our objects to.
+	//  This is because later we’ll want to “move the world”
+	//  so it looks like the user is moving.
+	//  https://threejs.org/docs/#api/en/objects/Group
+
+	world = new THREE.Group()
+	scene.add( world )
 
 
 	//  WebGL renderer.
@@ -96,8 +106,6 @@ function setupThree(){
 	renderer.setSize( window.innerWidth, window.innerHeight )
 	renderer.shadowMap.enabled = true
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap
-	renderer.physicallyCorrectLights = true
-	renderer.toneMapping = THREE.ACESFilmicToneMapping
 	renderer.outputEncoding = THREE.sRGBEncoding
 	renderer.xr.enabled = true
 	container.appendChild( VRButton.createButton( renderer ))
@@ -180,6 +188,7 @@ function setupThree(){
 //  would already be attached to window{}.
 //  https://developers.google.com/web/tools/chrome-devtools/console/javascript
 //  https://javascript.info/devtools
+//  Note: This is not something you’d want to do in production.
 
 window.THREE = THREE
 window.Handy = Handy
@@ -244,8 +253,6 @@ function setupHands(){
 
 
 	//  And here we go -- time for virtual reality hands!!
-	//  These models are not hosted on a CDN,
-	//  they’re included right in this code package.
 
 	const 
 	handModelFactory = new XRHandModelFactory(),
@@ -292,8 +299,7 @@ function setupHands(){
 		//
 		//      1 - A cube for each joint.
 		//      2 - A sphere for each joint.
-		//      3 - Low poly hand model.
-		//      4 - High poly hand model.
+		//      3 - High poly hand model.
 		//
 		//  Our intent is to display one at a time,
 		//  allowing the user to cycle through them
@@ -373,7 +379,7 @@ function setupHands(){
 			if( event.resultIs.pose.names.includes( 'peace' ) &&
 				!event.resultWas.pose.names.includes( 'peace' )){
 
-				console.log( 'Changing hand color for', hand.handedness )
+				console.log( 'Changing glove color for', hand.handedness )
 				hand.checkHandedness()
 				hand.traverse( function( obj ){
 
@@ -452,10 +458,10 @@ function setupContent() {
 
 		//  Every Mesh needs geometry; a collection of 3D points to use.
 		//  For this platform we’ll use some pre-defined geometry
-		//  that describes a circle:
-		//  https://threejs.org/docs/#api/en/geometries/CircleBufferGeometry
+		//  that describes a cylinder:
+		//  https://threejs.org/docs/#api/en/geometries/CylinderGeometry
 
-		new THREE.CircleBufferGeometry( 4, 12 ),
+		new THREE.CylinderGeometry( 4.5, 5, 1, 12 ),
 
 
 		//  For this Mesh we’ll use the “MeshStandardMaterial”.
@@ -468,22 +474,17 @@ function setupContent() {
 			color: 0xFFEECC,
 			roughness: 0.2,
 			metalness: 1.0,
-			envMapIntensity: 1.0,
-			transparent: true,
-			opacity: 1
+			emissive: 0xFFFFFF,
+			emissiveIntensity: 0.05
 		})
 	)
 
-	
-	//  In Three.js all flat 2D poses are drawn vertically.
-	//  This means that for any 2D pose 
-	//  that we’d like to use as a floor,
-	//  we  must rotate it 90 degrees (π ÷ 2 radians)
-	//  so that it is horizontal rather than vertical.
-	//  Here, we’ll rotate negatively (π ÷ -2 radians)
-	//  so the visible surface ends up on top.
-	
-	platform.rotation.x = Math.PI / -2
+
+	//  Objects are positioned according to their center
+	//  so we’d better force our platform downward
+	//  by half its height so its top is level to the floor.
+
+	platform.position.set( 0, -0.5, 0 )
 
 
 	//  By default meshes do not receive shadows.
@@ -495,39 +496,9 @@ function setupContent() {
 
 	//  And we want our platform to actually exist in our world
 	//  so we must add it to our scene.
+	//  Or that is… our “world” group within our scene.
 
-	scene.add( platform )
-
-
-	//  Environment map.
-	//  https://threejs.org/examples/webgl_materials_envmaps_exr.html
-	/*
-	const pmremGenerator = new THREE.PMREMGenerator( renderer )
-	pmremGenerator.compileCubemapShader()
-	THREE.DefaultLoadingManager.onLoad = function(){
-
-		pmremGenerator.dispose()
-	}
-	let cubeRenderTarget
-	new THREE.CubeTextureLoader()
-	.setPath( 'media/milkyway/' )
-	.load([ 
-
-		'dark-s_px.jpg', 
-		'dark-s_nx.jpg', 
-		'dark-s_py.jpg', 
-		'dark-s_ny.jpg', 
-		'dark-s_pz.jpg', 
-		'dark-s_nz.jpg' 
-	
-	], function( texture ){
-
-		texture.encoding = THREE.sRGBEncoding
-		const cubeRenderTarget = pmremGenerator.fromCubemap( texture )
-		platform.material.envMap = cubeRenderTarget.texture
-		platform.material.needsUpdate = true
-		texture.dispose()
-	})*/
+	world.add( platform )
 	
 
 	//  Let there be light.
@@ -535,15 +506,8 @@ function setupContent() {
 	//  https://threejs.org/docs/#api/en/lights/DirectionalLight
 
 	const light = new THREE.DirectionalLight( 0xFFFFFF )
-	light.position.set( -2, 4, 0 )
-	light.castShadow = true
-	light.shadow.camera.top    =  4
-	light.shadow.camera.bottom = -4
-	light.shadow.camera.right  =  4
-	light.shadow.camera.left   = -4
-	light.shadow.mapSize.set( 2048, 2048 )
+	light.position.set( -2, 8, 0 )
 	scene.add( light )
-	scene.add( new THREE.AmbientLight( 0x888888 ))
 
 
 	//  Lensflare !
@@ -577,8 +541,58 @@ function setupContent() {
 //////////////
 
 
-let timePrevious
+let 
+timePrevious,
+leftHandWalkPoseWas,
+rightHandWalkPoseWas
 
+window.walk = walk//  Just for testing!
+function walk(){
+
+	// console.log( '\n\nWALKING!\n\n' )
+	
+	const
+	position = new THREE.Vector3(),
+	rotation = new THREE.Quaternion(),
+	scale    = new THREE.Vector3(),
+	eulor    = new THREE.Euler()
+	
+	camera.matrixWorld.decompose( position, rotation, scale )
+
+
+	//  Get just the headset’s rotation around the Y axis.
+
+	eulor.setFromQuaternion( camera.getWorldQuaternion(), 'YXZ' )
+	console.log( 'eulor.y', eulor.y )
+
+
+	//  Sanity check (in degrees). 
+	//  Are we getting the FULL rotation range -180˚ to +180˚
+	//  instead of only -90˚ to +90˚.
+	//  THIS IS WHERE THE ERROR IS...
+	
+	// console.log( 'Headset Yaw:', ( eulor.y * THREE.Math.RAD2DEG ).toFixed( 2 ))
+	
+	
+	//  Apply the rotation angle and scale it back a bit.
+	//  The trackpad is returning values of -1 to +1
+	//  and if we applied “1” directly to our VR world
+	//  we’d be moving by one whole meter! So scale down!!
+
+	const
+	vector = new THREE.Vector3( 0, 0, -1 ),//  Start by looking straight ahead.
+	axis   = new THREE.Vector3( 0, 1, 0 ), //  Rotate around the Y-axis only.
+	angle  = eulor.y//  Take the Y-axis rotation from the headset.
+
+	
+	//  We’re going to rotate our straight-ahead vector
+	//  by taking the radians specified in eulor.y
+	//  and applying that to the Y-axis.
+
+	vector.applyAxisAngle( axis, angle )
+	vector.multiplyScalar( 0.5 )//  Let’s only move a little distance at a time.
+	world.position.sub( vector )
+}
 function loop( timeNow, frame ){
 
 	Handy.update( function( hand ){
@@ -616,6 +630,70 @@ function loop( timeNow, frame ){
 			}
 		}
 	})
+
+
+
+
+	//  Let’s get fancy and …
+	// “Let your fingers do the walking.”
+	// (Americans of a certain age might recall this
+	//  old Yellow Pages slogan and feel some nostalgia.)
+	//  NOTE: THIS IS VERY MUCH IN-PROGRESS AND HACKY!
+	//  I AM ASHAMED! DON’T LOOK AT ME!
+
+	const 
+	leftHand = Handy.hands.getLeft(),
+	rightHand = Handy.hands.getRight()
+
+	if( leftHand !== undefined && rightHand !== undefined ){
+
+
+		//  If this all seems verbose in terms of variable names
+		//  or the nested logic it’s for human legibility. 
+
+		window.bothHandsExist = true
+
+		const		
+		leftHandIsFlat = !!leftHand.isPose( 'flat', 8000 ),
+		leftHandIsWalkingIndex  = !!leftHand.isPose( 'walk index down', 5000 ),
+		leftHandIsWalkingMiddle = !!leftHand.isPose( 'walk middle down', 5000 ),
+		leftHandIsWalking = leftHandIsWalkingIndex || leftHandIsWalkingMiddle
+
+		const
+		rightHandIsFlat = !!rightHand.isPose( 'flat', 8000 ),
+		rightHandIsWalkingIndex  = !!rightHand.isPose( 'walk index down', 5000 ),
+		rightHandIsWalkingMiddle = !!rightHand.isPose( 'walk middle down', 5000 ),
+		rightHandIsWalking = rightHandIsWalkingIndex || rightHandIsWalkingMiddle
+
+		if(
+			( leftHandIsFlat && rightHandIsWalking ) || 
+			( leftHandIsWalking && rightHandIsFlat )){
+
+			window.aHandIsWalking = true
+			if( leftHandIsWalking ){
+			
+				window.leftHandIsWalking = true
+				if(
+					( leftHandIsWalkingIndex && leftHandWalkPoseWas === 'middle' ) ||
+					( leftHandIsWalkingMiddle && leftHandWalkPoseWas === 'index' )){
+					
+					walk()
+				}
+				leftHandWalkPoseWas = leftHandIsWalkingIndex ? 'index' : 'middle'
+			}
+			else if( rightHandIsWalking ){
+
+				window.rightHandIsWalking = true
+				if(
+					( rightHandIsWalkingIndex && rightHandWalkPoseWas === 'middle' ) ||
+					( rightHandIsWalkingMiddle && rightHandWalkPoseWas === 'index' )){
+					
+					walk()
+				}
+				rightHandWalkPoseWas = rightHandIsWalkingIndex ? 'index' : 'middle'
+			}
+		}
+	}
 
 
 	//  Determine the time since last frame in SECONDS (not milliseconds).
